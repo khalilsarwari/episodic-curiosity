05fe6f740c0ead454ee8312411928ded7c69383f
khalilsarwari <khalil.sarwari@berkeley.edu>
2020-11-15 13:49:17 -0800

    add missing breakout ec file

---
+++ experiments/ppo_eco_breakout.py
@@ -0,0 +1,11 @@
+from dotmap import DotMap
+from stable_baselines3 import PPO_ECO
+
+# Configuration for PPO with Episodic Curiosity on Breakout
+
+config = DotMap()
+config.agent = PPO_ECO
+config.environment = 'Breakout-v0'
+config.policy_model = 'MlpPolicy'
+config.tb_subdir = "ppo_eco_breakout"
+config.total_timesteps = 10000000
