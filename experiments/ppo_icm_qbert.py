8383fc56284feaef5675d6c65c13e3ab18f8d67d
Anusha Dandamudi <adandamudi@berkeley.edu>
2020-12-11 21:30:24 +0000

    Adding Qbert PPO_ICM experiment

---
+++ experiments/ppo_icm_qbert.py
@@ -0,0 +1,15 @@
+from dotmap import DotMap
+from stable_baselines3 import PPO_ICM
+
+# Configuration for PPO with Intrinsic Curiosity on QBert
+
+config = DotMap()
+config.agent = PPO_ICM
+config.environment = 'Qbert-v0'
+config.policy_model = 'CnnPolicy'
+config.tb_subdir = "ppo_icm_qbert"
+config.total_timesteps = 2e6
+config.action_shape = 6
+config.ensemble_size = 1
+config.atari_wrapper = True
+config.add_stoch = False
