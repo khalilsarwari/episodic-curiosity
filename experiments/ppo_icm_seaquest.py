525dccbd771afd6d5322862c807a728bf37945df
Anusha Dandamudi <adandamudi@berkeley.edu>
2020-12-13 19:03:10 +0000

    Adding seaquest experiment config for PPO ICM

--- experiments/ppo_icm_seaquest.py
+++ experiments/ppo_icm_seaquest.py
@@ -1,15 +1,15 @@
 from dotmap import DotMap
 from stable_baselines3 import PPO_ICM
 
-# Configuration for PPO with Intrinsic Curiosity on QBert
+# Configuration for PPO with Intrinsic Curiosity on Seaquest
 
 config = DotMap()
 config.agent = PPO_ICM
-config.environment = 'Qbert-v0'
+config.environment = 'Seaquest-v0'
 config.policy_model = 'CnnPolicy'
-config.tb_subdir = "ppo_icm_qbert"
+config.tb_subdir = "ppo_icm_seaquest"
 config.total_timesteps = 2e6
-config.action_shape = 6
+config.action_shape = 18
 config.ensemble_size = 1
 config.atari_wrapper = True
-config.add_stoch = True
+config.add_stoch = False
