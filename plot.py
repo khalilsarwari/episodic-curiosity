30caef99550c92c118d827f6e522f5d874852753
Anusha Dandamudi <adandamudi@berkeley.edu>
2020-12-13 23:01:32 -0800

    Updating plot script

--- plot.py
+++ plot.py
@@ -8,14 +8,22 @@ plt_dir = 'plots'
 if not os.path.exists(plt_dir):
     os.mkdir(plt_dir)
 
-def plot_mean_and_std(mean, std, line_name, title, xaxis='Iterations', yaxis='Extrinsic Reward per Episode', yrange=None):
+def plot_mean_and_std(means, stds, title, file_name, xaxis='Iterations', yaxis='Extrinsic Reward per Episode', yrange=None):
     """ Plot a mean field and its associated std """
-    mean = np.array(mean)
-    std = np.array(std)
-    t = list(range(len(mean)))
-    plt.plot(t,mean)
-    plt.fill_between(t, (mean-std), (mean+std), color='b', alpha=.1)
-    plt.savefig(os.path.join(plt_dir, f'{title}_plot.png'.replace(' ', '_')))
+    colors = ['r', 'g', 'b', 'c', 'm']
+    lines = []
+    for i in range(len(means)):
+        mean = np.array(means[i])
+        std = np.array(stds[i])
+        t = list(range(len(mean)))
+        line, = plt.plot(t,mean, color=colors[i])
+        lines.append(line)
+        plt.fill_between(t, (mean-std), (mean+std), color=colors[i], alpha=.1)
+    # plt.legend(lines, ('PPO', 'PPO_ICM', 'PPO_ECO', 'PPO_ICM_UNCERTAINTY', 'PPO_ECO_UNCERTAINTY'))
+    plt.xlabel("Number of Timesteps")
+    plt.ylabel("Reward")
+    plt.title(title)
+    plt.savefig(os.path.join(plt_dir, f'{file_name}_plot.png'.replace(' ', '_')))
 
 def plot_result(result, line_name, title, xaxis='Iterations', yaxis='Extrinsic Reward per Episode', yrange=None):
     """ Plot a regular/simple field, a single line """
@@ -34,5 +42,20 @@ def get_results(file, fields):
     return results
 
 if __name__ == "__main__":
-    results = get_results('old_logs/ppo_eco_breakout/PPO_ECO_3/events.out.tfevents.1606075072.AMS4.8895.0', ['rollout/ep_rew_mean', 'rollout/ep_rew_std'])
-    plot_mean_and_std(results['rollout/ep_rew_mean'], results['rollout/ep_rew_std'], line_name="Reward", title="Breakout PPO EC")
+    ####### Space Invaders, DETERMINISTIC #######
+    ppo = get_results('logs/space_invaders_deterministic/spaceinvaders_ppo_deterministic', ['rollout/ep_rew_mean', 'rollout/ep_rew_std'])
+    ppo_icm = get_results('logs/space_invaders_deterministic/spaceinvaders_ppoicm_deterministic', ['rollout/ep_rew_mean', 'rollout/ep_rew_std'])
+    ppo_eco = get_results('logs/space_invaders_deterministic/spaceinvaders_ppoeco_deterministic', ['rollout/ep_rew_mean', 'rollout/ep_rew_std'])
+    ppo_icm_unc = get_results('logs/space_invaders_deterministic/spaceinvaders_ppoicmuncertainty_deterministic', ['rollout/ep_rew_mean', 'rollout/ep_rew_std'])
+    ppo_eco_unc = get_results('logs/space_invaders_deterministic/spaceinvaders_ppoecouncertainty_deterministic', ['rollout/ep_rew_mean', 'rollout/ep_rew_std'])
+
+    means = [ppo['rollout/ep_rew_mean'], ppo_icm['rollout/ep_rew_mean'], \
+             ppo_eco['rollout/ep_rew_mean'], ppo_icm_unc['rollout/ep_rew_mean'], \
+             ppo_eco_unc['rollout/ep_rew_mean']]
+    stds = [ppo['rollout/ep_rew_std'], ppo_icm['rollout/ep_rew_std'], \
+            ppo_eco['rollout/ep_rew_std'], ppo_icm_unc['rollout/ep_rew_std'], \
+            ppo_eco_unc['rollout/ep_rew_std']]
+
+    plot_mean_and_std(means, stds, "Space Invaders, Deterministic", file_name="Space-Invaders-Deterministic")
+
+
